4c377f49afb1bc0896cb6e3b9a381b61a99680bc
马博渊 <793960628@qq.com>
2019-08-02 10:00:41 +0800

    master

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ def main(input_dir, output_dir):
     """
     sesf = SESF_Fuse()
     images_name = sorted(list({item[:-6] for item in os.listdir(input_dir)}))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     for image_name in images_name:
         print("Fusing {}".format(image_name))
         img1 = io.imread(os.path.join(input_dir, image_name + "_1.png"))
         img2 = io.imread(os.path.join(input_dir, image_name + "_2.png"))
         fused = sesf.fuse(img1, img2)
-        io.imsave(os.path.join(output_dir, "result", image_name + ".png"), fused)
+        io.imsave(os.path.join(output_dir, image_name + ".png"), fused)
 
 
 if __name__ == "__main__":
